0f79c94c4a49a4db159a946ee67cad716342702e
priyadarshini <priyadr@thoughtworks.com>
2015-08-28 18:22:31 +0530

    total tax calculation for shopping basket

--- src/com/tw/purchase/ShoppingBasket.java
+++ src/com/tw/purchase/ShoppingBasket.java
@@ -1,4 +1,22 @@
 package com.tw.purchase;
 
+import java.util.HashMap;
+
 public class ShoppingBasket {
+
+    HashMap<Good, Integer> items;
+
+    public ShoppingBasket() {
+        items = new HashMap<Good, Integer>();
+    }
+
+    public double totalTax() {
+        return 0;
+
+    }
+    public void addToBasket(Good good, int quantity) {
+        this.items.put(good, quantity);
+    }
+
+
 }
---
+++ test/com/tw/purchase/ShoppingBasketTest.java
@@ -0,0 +1,13 @@
+package com.tw.purchase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShoppingBasketTest {
+
+    @Test
+    public void shouldCalculateTaxForEmptyBasket() {
+        assertEquals(0,new ShoppingBasket().totalTax(),0.0);
+    }
+}
